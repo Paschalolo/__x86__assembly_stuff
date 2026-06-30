6fcfa8c6aa61f56b9e5918cb71e0d2b7b2d7085c
Paschalolo <pastechnology1@gmail.com>
2026-06-30 14:19:09 -0500

    update x86 sim DDD

--- src/t1.asm
+++ src/t1.asm
@@ -1,8 +1,6 @@
+;	Author : Paschal Ahanmisi 
+;Basic program simply printf out Hello everyone
 
-/*
-	Author : Paschal Ahanmisi 
-	Basic program simply printf out Hello everyone
-*/
 SECTION .data 
 	mydata db "Hello Everyone" , 0 
 	mydataLen equ $-mydata
@@ -10,4 +8,13 @@ SECTION .text
 	global _start 
 
 _start : 	
-	
+	mov rdi , 0x01 ; fd to 1
+	mov rsi , mydata 
+	mov rdx , mydataLen
+	mov rax , 0x01 ; syscall read 
+	syscall 
+
+	; temrination of the process 
+	xor rdi , rdi 
+	mov rax , 0x3c 
+	syscall
