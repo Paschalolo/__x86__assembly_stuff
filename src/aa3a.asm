abd0cf1b2e2eb74fb43a86a929da31d5b83ba612
Paschalolo <pastechnology1@gmail.com>
2026-09-08 07:26:17 -0500

    update x86 sim DDD

--- src/aa3a.asm
+++ src/aa3a.asm
@@ -15,6 +15,25 @@ SumvalsI32_a :
 	add eax , edx 
 	ret
 
+section .note.GNU-stack noalloc noexec nowrite progbits
 
+; int64_t SumvalsI32_mix (int32_t a , int8_t b , int32_t c , int64_t d , int16_t e , int32_t f , int8_t g, int64_t h); 
+SumvalsI32_mix : 
+	movsx rdi , edi 
+	movsx rsi , sil 
+	movsx rdx , edx
+	add rdi , rsi 
+	add rdx , rcx 
+	movsx r8 , r8w
+	movsx r9 , r9d 
+	add r8 , r9 
+	movsx r9 , byte[rsp+8]
+	add rdi , qword[rsp+16]
+	mov rax , rdi 
+	add rdx , r9 
+	add rax , rdx
+	ret
+
+	
 section .note.GNU-stack noalloc noexec nowrite progbits
 
