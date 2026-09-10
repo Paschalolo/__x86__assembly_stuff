0734a41706fcdcf568132ccc6732868ead9cfd15
Paschalolo <pastechnology1@gmail.com>
2026-09-09 20:37:51 -0500

    update x86 sim DDD

---
+++ src/pcie.cpp
@@ -0,0 +1,34 @@
+
+
+// Just building a pcie function packet 
+// it contains header of 64 bytes and 192 bytes of additonla information
+#include <cstddef>
+constexpr size_t DEVICE_ID_OFFSET = 0x00UL; 
+constexpr size_t VENDOR_ID_OFFSET = 0x02UL ; 
+constexpr size_t STATUS_OFFSET = 0x04UL ; 
+constexpr size_t COMMAND_ID_OFFSET = 0x06UL ; 
+constexpr size_t CLASSCODE_ID_OFFSET = 0x08UL ; 
+constexpr size_t REV_ID_OFFSET = 0x0BUL ; 
+constexpr size_t BIST_OFFSET = 0x0CUL ; 
+constexpr size_t HEADER_TY_OFFSET = 0x0DUL ; 
+constexpr size_t VENDOR_ID_OFFSET = 0x0EUL ; 
+constexpr size_t VENDOR_ID_OFFSET = 0x0FUL ; 
+constexpr size_t VENDOR_ID_OFFSET = 0x10UL ; 
+constexpr size_t VENDOR_ID_OFFSET = 0x02UL ; 
+constexpr size_t VENDOR_ID_OFFSET = 0x02UL ; 
+constexpr size_t VENDOR_ID_OFFSET = 0x02UL ; 
+constexpr size_t VENDOR_ID_OFFSET = 0x02UL ; 
+constexpr size_t VENDOR_ID_OFFSET = 0x02UL ; 
+constexpr size_t VENDOR_ID_OFFSET = 0x02UL ; 
+constexpr size_t VENDOR_ID_OFFSET = 0x02UL ; 
+constexpr size_t VENDOR_ID_OFFSET = 0x02UL ; 
+constexpr size_t VENDOR_ID_OFFSET = 0x02UL ; 
+constexpr size_t VENDOR_ID_OFFSET = 0x02UL ; 
+constexpr size_t VENDOR_ID_OFFSET = 0x02UL ; 
+constexpr size_t VENDOR_ID_OFFSET = 0x02UL ; 
+constexpr size_t VENDOR_ID_OFFSET = 0x02UL ; 
+constexpr size_t VENDOR_ID_OFFSET = 0x02UL ; 
+constexpr size_t VENDOR_ID_OFFSET = 0x02UL ; 
+constexpr size_t VENDOR_ID_OFFSET = 0x02UL ; 
+constexpr size_t VENDOR_ID_OFFSET = 0x02UL ; 
+
